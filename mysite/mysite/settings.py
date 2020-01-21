1b0ce5a5d5e23123adce602837bb6ed06449738a
Neko <nbadora@onet.eu>
2020-01-21 22:46:54 +0100

    fix1

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l*#rya@k6m39-60!l7^qj#yxtwq8c!im8!3!4u!rs)u-h$nff('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -29,3 +29,25 @@ urlpatterns = [
 
 # urlpatterns += staticfiles_urlpatterns()
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+
+# +++++++++++ DJANGO +++++++++++
+# To use your own django app use code like this:
+import os
+import sys
+
+# assuming your django settings file is at '/home/NekoNyanSekai/mysite/mysite/settings.py'
+# and your manage.py is is at '/home/NekoNyanSekai/mysite/manage.py'
+path = '/home/NekoNyanSekai/Projekt-Inzynierski/mysite'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+# then:
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
